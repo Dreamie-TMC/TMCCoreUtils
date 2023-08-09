3c60cb6a6dcc506ca27f15e66eb183bb6f54b410
Hailey <haileyhable@gmail.com>
2023-08-09 14:31:24 -0500

    Add tooltip constants

---
+++ TMC.Tools.CoreLib/Core/Ui/UiConstants.cs
@@ -0,0 +1,8 @@
+﻿namespace TMC.Tools.CoreLib.Core.Ui;
+
+public static class UiConstants
+{
+    public const int TooltipInitialShowDelayMs = 400;
+    public const int TooltipRepeatDelayMs = 400;
+    public const int TooltipDisplayLengthMs = 30000; //This is a hard limit of WinForms, sucks but oh well
+}
