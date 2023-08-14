32c16d7b6b5c69b19b6a85151d423e49e4022443
Hailey <haileyhable@gmail.com>
2023-08-13 20:24:18 -0500

    Add extra UI constants

--- TMC.Tools.CoreLib/Core/Ui/UiConstants.cs
+++ TMC.Tools.CoreLib/Core/Ui/UiConstants.cs
@@ -2,6 +2,16 @@
 
 public static class UiConstants
 {
+    public const int TopRowAboveSpacing = 15;
+    public const int FirstElementInRowX = 10;
+    public const int WidthMargin = 10;
+    public const int CategoryWidth = 760;
+    public const bool CheckboxesUseAutoEllipsis = true;
+    public const int CheckboxesPerRow = 3;
+    public const int RowHeight = 25;
+    public const int CheckboxWidth = 220;
+    public const int CheckboxHeight = 19;
+    
     public const int TooltipInitialShowDelayMs = 400;
     public const int TooltipRepeatDelayMs = 400;
     public const int TooltipDisplayLengthMs = 30000; //This is a hard limit of WinForms, sucks but oh well
