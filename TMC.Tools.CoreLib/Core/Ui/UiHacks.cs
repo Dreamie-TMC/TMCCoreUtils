fe1323818fa234fc8e208e2b1b92ac46fe7d530c
Hailey <haileyhable@gmail.com>
2023-08-13 20:24:06 -0500

    Add UI hacks to enable double buffering on lsit view

---
+++ TMC.Tools.CoreLib/Core/Ui/UiHacks.cs
@@ -0,0 +1,12 @@
+﻿using System.Reflection;
+
+namespace TMC.Tools.CoreLib.Core.Ui;
+
+public static class UiHacks
+{
+    public static void DoubleBuffering(this Control control, bool enable)
+    {
+        var method = typeof(Control).GetMethod("SetStyle", BindingFlags.Instance | BindingFlags.NonPublic);
+        method.Invoke(control, new object[] { ControlStyles.OptimizedDoubleBuffer, enable });
+    }
+}
