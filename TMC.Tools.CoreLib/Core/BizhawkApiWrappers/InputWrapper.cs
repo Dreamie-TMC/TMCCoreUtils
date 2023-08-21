4f3d1a1ed90f6192c18d230c8215b16c993b2c56
Hailey <haileyhable@gmail.com>
2023-08-21 10:01:55 -0500

    Add wrappers for keyboard/mouse input and joypad

---
+++ TMC.Tools.CoreLib/Core/BizhawkApiWrappers/InputWrapper.cs
@@ -0,0 +1,20 @@
+﻿namespace TMC.Tools.CoreLib.Core.BizhawkApiWrappers;
+
+public interface IInputWrapper
+{
+    IReadOnlyDictionary<string, object> GetMouse();
+    IReadOnlyList<string> GetInputs();
+}
+
+public class InputWrapper : IInputWrapper
+{
+    internal ApiContainerWrapper ApiContainerWrapper { get; set; }
+    
+    public InputWrapper(IApiContainerWrapper containerWrapper)
+    {
+        ApiContainerWrapper = (ApiContainerWrapper)containerWrapper;
+    }
+
+    public IReadOnlyDictionary<string, object> GetMouse() => ApiContainerWrapper.CurrentContainer.Input.GetMouse();
+    public IReadOnlyList<string> GetInputs() => ApiContainerWrapper.CurrentContainer.Input.GetPressedButtons();
+}
---
+++ TMC.Tools.CoreLib/Core/BizhawkApiWrappers/JoypadWrapper.cs
@@ -0,0 +1,27 @@
+﻿namespace TMC.Tools.CoreLib.Core.BizhawkApiWrappers;
+
+public interface IJoypadWrapper
+{
+    IReadOnlyDictionary<string, object> GetInputs(int? controllerNum = null);
+    IReadOnlyDictionary<string, object> GetInputsWithMovie(int? controllerNum = null);
+
+    void SetInputs(IReadOnlyDictionary<string, bool> inputs, int? controllerNum = null);
+
+    IReadOnlyDictionary<string, object> GetInputsImmediate(int? controllerNum = null);
+}
+
+public class JoypadWrapper : IJoypadWrapper
+{
+    internal ApiContainerWrapper ApiContainerWrapper { get; set; }
+    
+    public JoypadWrapper(IApiContainerWrapper apiContainerWrapper)
+    {
+        ApiContainerWrapper = (ApiContainerWrapper)apiContainerWrapper;
+    }
+
+    public IReadOnlyDictionary<string, object> GetInputs(int? controllerNum = null) => ApiContainerWrapper.CurrentContainer.Joypad.Get(controllerNum);
+    public IReadOnlyDictionary<string, object> GetInputsWithMovie(int? controllerNum = null) => ApiContainerWrapper.CurrentContainer.Joypad.GetWithMovie(controllerNum);
+    public IReadOnlyDictionary<string, object> GetInputsImmediate(int? controllerNum = null) => ApiContainerWrapper.CurrentContainer.Joypad.GetImmediate(controllerNum);
+
+    public void SetInputs(IReadOnlyDictionary<string, bool> inputs, int? controllerNum = null) => ApiContainerWrapper.CurrentContainer.Joypad.Set(inputs, controllerNum);
+}
