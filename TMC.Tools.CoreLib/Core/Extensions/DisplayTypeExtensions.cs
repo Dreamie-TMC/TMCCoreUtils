f1aac9221e5fd0eeff5bb1ecfaa960419d507451
Hailey <haileyhable@gmail.com>
2024-06-14 15:15:49 -0500

    Add display type and display type extensions

---
+++ TMC.Tools.CoreLib/Core/Extensions/DisplayTypeExtensions.cs
@@ -0,0 +1,19 @@
+﻿using TMC.Tools.CoreLib.Core.Enums;
+
+namespace TMC.Tools.CoreLib.Core.Extensions;
+
+public static class DisplayTypeExtensions
+{
+    public static string ConvertTypeToString(this DisplayType type) => type switch
+    {
+        DisplayType.Binary => "Binary",
+        DisplayType.Byte => "Byte",
+        DisplayType.HalfWord => "2 Bytes",
+        DisplayType.Word => "4 Bytes",
+        DisplayType.ByteArray => "Byte Array",
+        DisplayType.FixedPointHalfWord => "Fixed (2 Bytes)",
+        DisplayType.FixedPointWord => "Fixed (4 Bytes)",
+        DisplayType.String => "String",
+        _ => "Invalid Type"
+    };
+}
